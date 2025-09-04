ac3a19f0df9386daf2a322f782fd923ce16b4dc6
Caesar <caesar.keene@gmail.com>
2025-09-04 15:55:37 +0300

    gone through strings and numbers

--- refresher.py
+++ refresher.py
@@ -1,4 +1,4 @@
-# Day 1 Variables 
+# Variables and Syntax 
 message = 'Hello, World!'
 print(message)
 
@@ -7,8 +7,62 @@ print(message)
 
 variable_name = 'caesar' 
 
+# concatenate 
 new_message = message + variable_name
 print(new_message)  
 
+# Strings 
 
+# A string with double quotes should use single quotes 
+another_message = '"We are on day 2 today" he said.'
+print(another_message)
+
+# multiline string
+help_message = ''' 
+Password is wrong!
+Hint: dog's name 
+''' 
+print(help_message)
+
+# Numbers: integers 
+x = 20
+y = 3
+
+total = x + y
+print(total)
+
+difference = x - y
+print(difference)
+
+product = x * y
+print(product)
+
+# quotient in python gets the remainder of the two numbers being divided
+quotient = x / y
+print(quotient)
+
+# Power of a number 
+a = 4 
+c = 4 
+power = a**c
+print(power)
+
+
+
+# Numbers: floats 
+# Floats are any number that have a decimal point 
+
+x = 0.5
+y = 0.25
+
+total = x + y
+print(total)
+
+difference = x - y
+print(difference)
+
+product = x * y
+print(product)
+
+# 
 
