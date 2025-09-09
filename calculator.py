a6616ee79b57afb80d104d26c0b5d743871bbbcb
Caesar <caesar.keene@gmail.com>
2025-09-09 15:39:31 +0300

    VAT rate calculator using int and float

--- calculator.py
+++ calculator.py
@@ -1,11 +1,30 @@
+"""
 #calculator using int function for whole numbers
+
 num1 = input("Enter a number: ") 
 num2 = input("Enter another number: ")
 result = int(num1) + int(num2)
 print(result) 
 
+"""
+
+"""
 #calculator using float function for decimal numbers
 num1 = input("Enter a number: ") 
 num2 = input("Enter another number: ")
 result = float(num1) + float(num2)
 print(result)  
+"""
+
+# VAT rate calculation
+price = input("Enter price of product (Ksh): ")
+tax = input("Enter VAT percentage (%): ")
+
+VAT_amount = int(price) * float(tax) 
+final_amount = VAT_amount + int(price)
+print(final_amount)
+
+
+
+
+
