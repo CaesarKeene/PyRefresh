e7decf3877bbe5bdee84ef2ad4fc705ad193fe99
Caesar <caesar.keene@gmail.com>
2025-09-09 14:04:39 +0300

    calculator with float function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
-#calculator using function names
+#calculator using int function for whole numbers
 num1 = input("Enter a number: ") 
 num2 = input("Enter another number: ")
 result = int(num1) + int(num2)
 print(result) 
+
+#calculator using float function for decimal numbers
+num1 = input("Enter a number: ") 
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)  
