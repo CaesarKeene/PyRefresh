dfa5cd9307b52d035b4dee306bbb72fa2ed42f05
Caesar <caesar.keene@gmail.com>
2025-09-09 16:24:55 +0300

    addition of formatted string literal

--- calculator.py
+++ calculator.py
@@ -1,6 +1,5 @@
-"""
-#calculator using int function for whole numbers
 
+#calculator using int function for whole numbers
 num1 = input("Enter a number: ") 
 num2 = input("Enter another number: ")
 result = int(num1) + int(num2)
@@ -11,19 +10,19 @@ num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
 result = float(num1) + float(num2)
 print(result)  
-"""
+
 
 # VAT rate calculation 
 """ This is a small VAT caluculator that uses both int and float data types to get 
-the price of a product including VAT 
+the price of a product including VAT. 
+With the addition of a formatted string literal for cleaner code.
 """
 price = input("Enter price of product (Ksh): ")
 tax = input("Enter VAT percentage (%): ")
 
 VAT_amount = int(price) * float(tax) 
 final_amount = VAT_amount + int(price)
-print(final_amount)
-
+print(f"The price of your product is Ksh: {final_amount}")
 
 
 
