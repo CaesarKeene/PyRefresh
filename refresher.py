999a9f29d4cb8fb54db8627729d3bb3988cfe389
Caesar <caesar.keene@gmail.com>
2025-09-11 17:28:24 +0300

    if else statements

--- refresher.py
+++ refresher.py
@@ -93,8 +93,8 @@ else:
 
 """
 # if/else statements 
-age = input("Enter your age")
-if int(age) < 18
-
-
-
+age = input("Enter your age: ")
+if int(age) >= 18:
+    print("You are eligible to vote")
+else: 
+    print("You are not eligible to vote ") 
