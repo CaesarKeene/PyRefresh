30e308995b881aa75ed083d62826ecd3ab26c52d
Caesar <caesar.keene@gmail.com>
2025-09-16 13:14:55 +0300

    ternary operators

--- refresher.py
+++ refresher.py
@@ -102,10 +102,11 @@ else:
     print("You are not eligible to vote ") 
 """
 
+"""
 # if else elif statements 
 age = input("Enter your age:")
 user_age = int(age)
-"""
+
 if user_age < 12:
     ticket_price = 100
 elif user_age <= 17:
@@ -113,7 +114,9 @@ elif user_age <= 17:
 elif user_age >= 18:
     ticket_price = 600 
 print(f"Your ticket price is Ksh:{ticket_price}") 
-"""
+
+age = input("Enter your age:")
+user_age = int(age)
 
 if user_age < 5:
     ticket_price = 100
@@ -122,3 +125,11 @@ elif user_age < 17:
 else:
     ticket_price = 600                      #else should be the last resort or condition
 print(f"Your ticket price is Ksh:{ticket_price}") 
+
+"""
+
+# ternary operator 
+user_age = input("Enter your age: ")
+user_age = 25
+ticket_price = 100 if int(user_age) >= 25 else 50 
+print(f"Your ticket price is {ticket_price}")
