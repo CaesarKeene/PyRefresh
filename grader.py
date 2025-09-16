d337239a9f2c83b20faa311d408a386e9876d257
Caesar <caesar.keene@gmail.com>
2025-09-16 17:00:30 +0300

    added grading conditions

---
+++ grader.py
@@ -0,0 +1,16 @@
+# this is a grader that asks for a students grade 
+
+print("Hello, welcome to your grade calculator! ")
+grade = input("Please enter your grade: ") 
+grade_no = int(grade) 
+
+if grade_no >= 90:
+    print("A")
+elif grade_no >= 80:
+    print("B")
+elif grade_no >= 70:
+    print("C")
+elif grade_no >= 60:
+    print("You have a referral, kindly see the HOD ") 
+
+grade_no 
