79503816cbfbdef6af69d56b53a8941eef374341
Caesar <caesar.keene@gmail.com>
2025-09-17 15:48:55 +0300

    changed the code with added comments

--- grader.py
+++ grader.py
@@ -1,9 +1,11 @@
 # this is a grader that asks for a students grade 
 
+#welcome message and input for the user 
 print("Hello, welcome to your grade calculator! ")
 grade = input("Please enter your grade: ") 
 grade_no = int(grade) 
 
+# These are the grade conditions 
 if grade_no >= 80:
     print("A")
 elif grade_no >= 70:
@@ -11,9 +13,14 @@ elif grade_no >= 70:
 elif grade_no >= 60:
     print("C")
 elif grade_no >= 50:
-    print("You have a clear pass ") 
+    print("Pass ")     
+elif grade_no >= 40:
+    print("You have a clear pass ")
+elif grade_no >= 20:
+    print("Refferal - see HOD")
 
-grade_no = print("You have a referral, kindly see the HOD! ") if grade_no >= 40 else print("Fail")
-
+else: 
+    print("Fail")   
+ 
 
 
